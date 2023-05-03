b41aac24692107d59e17dae16b3aaddc81adee6f
Kamadarada <felipekamada2@gmail.com>
2023-05-02 23:16:53 -0300

    Criação do método de consulta Criação do foreach para listar clientes nas tabelas. Criação de API de busca de CEP e preenchimento de campos automatico

--- Projeto_SolCar/wwwroot/js/site.js
+++ Projeto_SolCar/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const inputCEP = document.getElementById('inputCEP')
+const inputRua = document.getElementById('inputRua')
+const inputCidade = document.getElementById('inputCidade')
+const inputBairro = document.getElementById('inputBairro')
+const inputEstado = document.getElementById('inputEstado')
+
+
+inputCEP.addEventListener('input', (e) => {
+    CEP = e.target.value
+    console.log(CEP)
+
+    if (CEP.length == 8) {
+        BuscaCep(CEP)
+    }
+
+    else{
+        inputRua.value = ""
+        inputCidade.value = ""
+        inputBairro.value = ""
+        inputEstado.value = ""
+    }
+});
+
+
+async function BuscaCep(CEP){
+
+    const APIfetch = await fetch(`https://brasilapi.com.br/api/cep/v1/${CEP}`)
+    const API = await APIfetch.json()
+    console.log(API)
+
+    inputRua.value = API.street;
+    inputCidade.value = API.city;
+    inputBairro.value = API.neighborhood;
+    inputEstado.value = API.state;
+}
+
+
+
+
