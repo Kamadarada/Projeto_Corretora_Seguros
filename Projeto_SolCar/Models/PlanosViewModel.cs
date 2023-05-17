ec1dea3921240c6fa383f36bc2c8b0d2b47e608d
Kamadarada <felipekamada2@gmail.com>
2023-05-17 16:23:02 -0300

    Planos view model com as collections de seguro casa e seguro carro
    Alteração da model de "details"

---
+++ Projeto_SolCar/Models/PlanosViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Projeto_SolCar.Entidades;
+
+namespace Projeto_SolCar.Models
+{
+    public class PlanosViewModel : Clientes
+    {
+        public ICollection<SeguroCasa> Seguro_Casa { get; set; }
+
+        public ICollection<SeguroCarro> Seguro_Carro { get; set; }
+    }
+}
